02e9b36bf011fae98adfaba7befd11bb9d0a6606
Sujen Shah <sujen1412@gmail.com>
2025-02-26 00:02:36 -0800

    Split element as arguments provided to run script

--- .maap/run.sh
+++ .maap/run.sh
@@ -7,4 +7,4 @@ root_dir=$(dirname "${basedir}")
 INPUT_FILE=$(ls -d input/*)
 mkdir -p output
 source activate ingest
-python "${root_dir}"/src/ingest.py -i "${INPUT_FILE}" --variables "${@:1}" -o output
+python "${root_dir}"/src/ingest.py -i "${INPUT_FILE}" --variables ${@:1} -o output
