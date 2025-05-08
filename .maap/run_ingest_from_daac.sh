2af632a75aa7f2d6c2a6d6dec6d41ea5cc336018
bsatoriu <bsatoriu@jpl.nasa.gov>
2025-05-08 15:24:41 -0700

    environment.yml update; parameritize run command

--- .maap/run_ingest_from_daac.sh
+++ .maap/run_ingest_from_daac.sh
@@ -3,8 +3,18 @@
 # Get current location of build script
 basedir=$( cd "$(dirname "$0")" ; pwd -P )
 root_dir=$(dirname "${basedir}")
+granule_id="$1"
+collection_id="$2"
+s3_bucket="$3"
+s3_prefix="$4"
+role_arn="$5"
 
-INPUT_FILE=$(ls -d input/*)
 mkdir -p output
 source activate ingest
-python "${root_dir}"/src/stage_from_daac.py -i "${INPUT_FILE}" --variables ${@:1} -o output
+
+python "${root_dir}"/src/stage_from_daac.py \
+    --granule-id "${granule_id}" \
+    --collection-id "${collection_id}" \
+    --s3-bucket "${s3_bucket}" \
+    --s3-prefix "${s3_prefix}" \
+    --role-arn "${role_arn}"
