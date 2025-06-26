610f52d9898410f4c4499522247478ed5916cd65
Sujen Shah <sujen1412@gmail.com>
2025-06-25 22:11:53 -0700

    Null check

--- src/pipeline_generic.py
+++ src/pipeline_generic.py
@@ -85,7 +85,7 @@ def validate_arguments(args):
 
 def detect_input_type(args):
     """Detect the type of input and processing needed"""
-    if len(args.granule_id) > 0:
+    if args.granule_id and len(args.granule_id) > 0:
         return "daac"
     elif args.input_s3:
         if args.input_s3.endswith(('.nc', '.nc4')):
