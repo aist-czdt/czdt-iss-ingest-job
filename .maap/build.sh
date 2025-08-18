0059a8cd23ada490696ff2b5d766ec688c562edb
rileykk <rileykk@jpl.nasa.gov>
2025-08-18 08:35:29 -0700

    set e in build

--- .maap/build.sh
+++ .maap/build.sh
@@ -2,7 +2,9 @@
 
 # Get current location of build script
 basedir=$( cd "$(dirname "$0")" ; pwd -P )
-root_dir=$(dirname "${basedir}")
+root_dir=$(dirname "${basedir}")+
+
+set -e
 
 pushd "${root_dir}"
 conda env update -f environment.yml
